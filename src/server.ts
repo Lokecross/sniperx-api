fc7889e87cd4ddbcc5e872544db7875023ed6da6
lucas.marcelino <lucas.marcelino.silva@live.com>
2022-11-09 22:14:01 -0300

    console

--- src/server.ts
+++ src/server.ts
@@ -18,36 +18,50 @@ route.get('/', (req: Request, res: Response) => {
 
 // order.created - Disparado ao criar novo pedido
 route.post('/order/created', (req: Request, res: Response) => {
+  console.log(req.body);
+
   res.json({ success: true, content: req.body })
 })
 
 // order.approved - Disparado ao pedido ser pago e aprovado
 route.post('/order/approved', (req: Request, res: Response) => {
+  console.log(req.body);
+
   res.json({ success: true, content: req.body })
 })
 
 // order.payment_failed - Disparado ao pedido ter um pagamento falho
 route.post('/order/payment_failed', (req: Request, res: Response) => {
+  console.log(req.body);
+
   res.json({ success: true, content: req.body })
 })
 
 // subscription.past_due - Disparado quando uma assinatura está atrasada
 route.post('/subscription/past_due', (req: Request, res: Response) => {
+  console.log(req.body);
+
   res.json({ success: true, content: req.body })
 })
 
 // subscription.canceled - Disparado quando a assinatura for cancelada
 route.post('/subscription/canceled', (req: Request, res: Response) => {
+  console.log(req.body);
+
   res.json({ success: true, content: req.body })
 })
 
 // subscription.approved - Disparado quando a assinatura for cancelada
 route.post('/subscription/approved', (req: Request, res: Response) => {
+  console.log(req.body);
+
   res.json({ success: true, content: req.body })
 })
 
 // cart.abandon - Disparado quando o carrinho for abandonado
 route.post('/cart/abandon', (req: Request, res: Response) => {
+  console.log(req.body);
+
   res.json({ success: true, content: req.body })
 })
 
