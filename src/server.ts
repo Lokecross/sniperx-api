b435cbb27e8024a7a86988e7dd5001fd9071ada8
lucas.marcelino <lucas.marcelino.silva@live.com>
2022-11-09 22:07:34 -0300

    port typo

--- src/server.ts
+++ src/server.ts
@@ -48,6 +48,6 @@ route.post('/cart/abandon', (req: Request, res: Response) => {
 
 app.use(route)
 
-const port = process.env.POST || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => `server running on port ${port}`);
